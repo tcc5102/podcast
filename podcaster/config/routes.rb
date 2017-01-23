82e8c9d51d91b25307b8d1d8bf8db0703d4a3ced
Tyler Cooper <tyler.cooper91@gmail.com>
2017-01-23 15:40:28 -0500

    begin user view, install bootstrap

--- podcaster/config/routes.rb
+++ podcaster/config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'user/show'
+
   devise_for :users
-  root to: "home#index"
+
+  devise_scope :user do
+    authenticated :user do
+      # go to user show page upon sign in
+      root 'user#show', as: :authenticated_root
+    end
+
+    unauthenticated do
+      # login page if not signed in
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  # resources :users, only: [:show] do
+  #   resources :items, only: [:create, :destroy]
+  # end
+
 end
